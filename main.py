eb98252c61dac69a3547819ff6658cce1c686b98
Lion-Ga92 <Luis@tamalesgarcia.com>
2020-06-16 01:16:35 -0700

    Final touches to test

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ print("Your day one tasks are...")
 day_one.list_tasks()
 print("Your day two tasks are...")
 day_two.list_tasks()
+
+print("Start checkng off tasks: " + "\n")
+day_one.check_for_tasks()
+print("Your day two tasks to check off are: " + "\n")
+day_two.check_for_tasks()
+
